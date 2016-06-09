8a625354b2c8f1be9cdc6516998d0382e12071f1
yohey5273 <k14jk076@st.kyusan-u.ac.jp>
2016-06-09 17:32:28 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include('page_header.php');
+?>
+
+<h1 class="bg-danger">First Git Project</h1>
+
+<?php
+include('page_footer.php');
+?>
---
+++ page-header.php
@@ -0,0 +1,5 @@
+<?php
+ echo '<html><head>';
+ echo '<meta http-equiv="Content-TYPE" content="text/html; charset=UTF-8"></head>';
+ echo '<body>';
+?>
